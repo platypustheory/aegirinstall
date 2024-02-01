043d8f9e68ff12be738fd35e23f74ffbc3337f7b
Dan Friedman <dan@consensus.enterprises>
2024-02-01 03:48:13 +0000

    Elevate perms as needed, ensure we update mysql root password on ansible runs, quote vars in playbook, actually run the playbook.

--- minion/minion-up.sh
+++ minion/minion-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
-apt-get update -yqq
-apt-get install -yqq git make unzip
-apt-get install --assume-yes ansible
+sudo apt-get update -yqq
+sudo apt-get install -yqq git make unzip
+sudo apt-get install --assume-yes ansible
 mkdir -p roles
 git clone --recursive https://gitlab.com/consensus.enterprises/ansible-roles/ansible-role-aegir-minion roles/consensus.aegir-minion
 git clone --recursive https://gitlab.com/consensus.enterprises/ansible-roles/ansible-role-mysql roles/consensus.mysql
@@ -9,4 +9,4 @@ git clone https://gitlab.com/consensus.enterprises/ansible-roles/ansible-role-ng
 git clone https://github.com/consensus-enterprises/ansible-role-php.git roles/consensus.php
 git clone https://github.com/consensus-enterprises/ansible-role-php-versions.git roles/consensus.php-versions
 ansible-galaxy collection install community.crypto community.general community.mysql
-echo "ansible-playbook minion.yml --connection=local -i inventory"
+ansible-playbook minion.yml --connection=local -i inventory
