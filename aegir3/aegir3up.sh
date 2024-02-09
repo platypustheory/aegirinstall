b3781d56dc32446a32ebafe2454e059e504d43cc
Dan Friedman <dan@consensus.enterprises>
2024-02-09 11:58:33 -0500

    Fix script to match git roles playbook filename.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -4,5 +4,5 @@ sudo apt-get install -yqq git make unzip software-properties-common acl
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -yqq ansible
 ansible-galaxy install -r requirements.yaml
-ansible-playbook -i inventory -c local roles.yaml
+ansible-playbook -i inventory -c local git-roles.yaml
 echo "Done! Next: ansible-playbook aegir3.yaml --connection=local -i inventory"
