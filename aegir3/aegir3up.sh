33f8d48b05cf7e77b435085c8b53c905a4c3b0ac
Dan Friedman <dan@consensus.enterprises>
2024-02-09 12:15:28 -0500

    Improve workflow and comments.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -4,5 +4,7 @@ sudo apt-get install -yqq git make unzip software-properties-common acl
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -yqq ansible
 ansible-galaxy install -r requirements.yaml
+# Check the roles we need out of github/gitlab:
 ansible-playbook -i inventory -c local git-roles.yaml
-echo "Done! Next: ansible-playbook aegir3.yaml --connection=local -i inventory"
+# Install Aegir:
+ansible-playbook -i inventory -c local aegir3.yaml
