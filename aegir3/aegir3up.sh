2f0ed59e2336760c0f16e1f15ccdd9f97b2e7474
lamech <27463+lamech@users.noreply.github.com>
2024-11-08 16:53:30 +0000

    Updates for un-forking geerlingguy Ansible roles.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -v
+#!/bin/bash
+set -e
+set -x
 sudo apt-get update -yqq
 sudo apt-get install -yqq git make unzip software-properties-common acl
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -yqq ansible
 ansible-galaxy install -r requirements.yaml
-# Check the roles we need out of github/gitlab:
-ansible-playbook -i inventory -c local git-roles.yaml
+mkdir -p roles
+git clone --recursive -b 8-meta-unfork-dependent-roles-where-possible https://gitlab.com/consensus.enterprises/ansible-roles/ansible-role-aegir roles/ansible-role-aegir
 # Install Aegir:
 ansible-playbook -i inventory -c local aegir3.yaml
