d782b0f5e62a2301a7f83e64a55869df7d7756e1
Dan Friedman <dan@consensus.enterprises>
2024-11-11 15:03:32 +0000

    Until it gets published on Galaxy, we still need to check out the Consensus Ansible role in a separate step. The rest have been un-forked back to geerlingguy.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 set -x
-sudo apt-get update -yqq
-sudo apt-get install -yqq git make unzip software-properties-common acl
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -yqq ansible
-ansible-galaxy install -r requirements.yaml
-mkdir -p roles
-git clone --recursive -b 8-meta-unfork-dependent-roles-where-possible https://gitlab.com/consensus.enterprises/ansible-roles/ansible-role-aegir roles/ansible-role-aegir
+#sudo apt-get update -yqq
+#sudo apt-get install -yqq git make unzip software-properties-common acl
+#sudo add-apt-repository --yes --update ppa:ansible/ansible
+#sudo apt-get install -yqq ansible
+#ansible-galaxy install -r requirements.yaml
+# Check out Consensus Aegir role:
+ansible-playbook -i inventory -c local git-roles.yaml
 # Install Aegir:
 ansible-playbook -i inventory -c local aegir3.yaml
