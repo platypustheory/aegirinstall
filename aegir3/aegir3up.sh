1caca4eb1d67c91d7cf20efe28006fe7ea93b625
Dan Friedman <dan@consensus.enterprises>
2024-11-11 15:25:41 +0000

    Revert erroneous commentings-out.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 set -x
-#sudo apt-get update -yqq
-#sudo apt-get install -yqq git make unzip software-properties-common acl
-#sudo add-apt-repository --yes --update ppa:ansible/ansible
-#sudo apt-get install -yqq ansible
-#ansible-galaxy install -r requirements.yaml
+sudo apt-get update -yqq
+sudo apt-get install -yqq git make unzip software-properties-common acl
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -yqq ansible
+ansible-galaxy install -r requirements.yaml
 # Check out Consensus Aegir role:
 ansible-playbook -i inventory -c local git-roles.yaml
 # Install Aegir:
