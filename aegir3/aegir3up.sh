ee1168db179d36592f734091117b412629a064d5
Dan Friedman <dan@consensus.enterprises>
2024-11-22 10:09:27 -0500

    Complete migration to Galaxy roles.

--- aegir3/aegir3up.sh
+++ aegir3/aegir3up.sh
@@ -6,7 +6,5 @@ sudo apt-get install -yqq git make unzip software-properties-common acl
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -yqq ansible
 ansible-galaxy install -r requirements.yaml
-# Check out Consensus Aegir role:
-ansible-playbook -i inventory -c local git-roles.yaml
 # Install Aegir:
 ansible-playbook -i inventory -c local aegir3.yaml
